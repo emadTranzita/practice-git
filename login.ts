fee11cc46e1d323178d5074d44ce36422e29fd05
emadTranzita <emad@tranzita.com>
2026-08-25 13:28:39 +0530

    add login handler

--- login.ts
+++ login.ts
@@ -1,2 +1,14 @@
-Editing from feature-login branch
-hello world
+// Editing from feature-login branch
+
+function handleLogin (username: string, email: string, password: string) {
+
+	if (!username || !email || !password) {
+		return;
+	}
+
+	console.log("successfully loged in!!");
+
+}
+
+handleLogin("emad", "emad@emad.com", "emad@111");
+
