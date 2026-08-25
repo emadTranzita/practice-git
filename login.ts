71f691620e7d7baa80a18cb9b2e041173b154846
emadTranzita <emad@tranzita.com>
2026-08-25 12:50:54 +0530

    clean file

--- login.ts
+++ login.ts
@@ -1,12 +1,1 @@
-function handleLogin(usernam: string, email: string, password: string) {
-	if (!usename || !email || !password) {
 
-		return;
-	}
-
-	console.log("successfully logged in!");
-
-}
-
-
-handleLogin("emad", "emad@emad.com", "emad@111");
